5b6e0a166181e902ced5ed6b7f33c3ca272bd176
localhost <dariusz.slusar@upaid.pl>
2017-11-12 15:01:49 +0100

    algo2

--- algorytmy.py
+++ algorytmy.py
@@ -10,8 +10,36 @@ def search():
         return number
     else:
         return None
-
-print(search())
 ##########################################
-#Binarcy search
+
+#Binary search
+def binarySearch():
+    tab = []
+    for i in range(10):
+        tab.append(random.randint(0,100))
+    tab.sort()
+    print("tab: ", tab)
+    l = 0
+    p = len(tab)-1
+    print(p)
+    liczba = int(input("Number: "))
+    if l > p:
+        return None
+    else:
+        s = (l+p)//2
+        if liczba == tab[s]:
+            print("znaleziono")
+        elif liczba < tab[s]:
+            p = s -1
+            print("w pierwszych:", p)
+            return p
+        elif liczba > tab[s]:
+            l = s + 1
+            print("od wartosci tbalicy; ", l)
+##############################################################
+
+
+
+
+
 
